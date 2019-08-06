5c0c4d9d8c5b8ce8766864ed3ca3f9d93af1014c
Stephen Malito <stevemalito@gmail.com>
2019-08-06 17:21:44 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos'
+import './App.css';
+
+
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+      id:1,
+      title: "Wakeup",
+      completed: false
+    },
+    {
+      id:2,
+      title: "Dinner",
+      completed: false
+    },
+    {
+      id:3,
+      title: "Sleep",
+      completed: false
+    },
+  ]
+  }
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) })
+}
+  render() { 
+    // console.log(this.state.todos)
+    return (
+        <div className="App">
+          <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+        </div>
+      );
+
+  }
+
+}
+export default App;
+// function App(props) {
+  
+//   console.log(state.todos)
+//   return (
+//     <div className="App">
+//       <Todos todos={this.state.todos}/>
+//     </div>
+//   );
+// }
---
+++ src/components/Todos.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem'
+import PropTypes from 'prop-types';
+
+
+class Todos extends Component {
+ 
+
+   render() {
+    //    console.log(this.props.todos)
+    return this.props.todos.map((todo) =>(
+        // <h3>{ todo.title}</h3>
+        <TodoItem key={todo.id} todo ={todo} markComplete={this.props.markComplete}/>
+    ));
+   }
+  };
+
+//proptypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+export default Todos;
+//        return this.props.todos.map((todo) => (
+//            <h3>{}</h3>
+//        )
+       
+//         );
