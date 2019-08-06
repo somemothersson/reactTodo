056be086635e10789bfa1732e2e4d4ae9d018e40
Stephen Malito <stevemalito@gmail.com>
2019-08-06 17:51:44 -0500

    submit new todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/Layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 import './App.css';
 
 
@@ -36,12 +38,14 @@ class App extends Component {
 
 //Delete Todo
 delTodo = (id) => {
-  console.log(id)
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !==id)]})
 }
   render() { 
     // console.log(this.state.todos)
     return (
         <div className="App">
+          <Header />
+          <AddTodo />
           <Todos 
             todos={this.state.todos} 
             markComplete={this.markComplete}
